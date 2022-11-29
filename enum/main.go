5def98f98b1a5d5c709fc9addaf38579ab70bc87
shunya.inoue <shunya.inoue@m0mentum.co.jp>
2022-11-29 09:27:14 +0900

    create enum

---
+++ enum/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type Fruit int
+
+const (
+	Unknown Fruit = iota
+	Apples
+	Bananas
+	Cherries
+	Durian
+	Elderberry
+)
+
+// use stringer: https://pkg.go.dev/fmt#Stringer
+func (f Fruit) String() string {
+	switch f {
+	case Apples:
+		return "Apples"
+	case Bananas:
+		return "Bananas"
+	case Cherries:
+		return "Cherries"
+	case Durian:
+		return "Durian"
+	case Elderberry:
+		return "Elderberry"
+	default:
+		return "Unknown"
+	}
+}
+
+func main() {
+	fmt.Println(Apples)
+	fmt.Println(Durian)
+	fmt.Println(2)
+	fmt.Println(100)
+
+	var kiwi Fruit = 11
+
+	fmt.Println(kiwi)
+}
